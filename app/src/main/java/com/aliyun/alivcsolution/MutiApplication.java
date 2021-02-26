f4efc7db88bd08d5329cec7269f08c6026598c10
YaronWong <yaron.wong@gmail.com>
2021-02-26 11:04:08 +0800

    更改目录结构

--- app/src/main/java/com/aliyun/alivcsolution/MutiApplication.java
+++ app/src/main/java/com/aliyun/alivcsolution/MutiApplication.java
@@ -7,24 +7,13 @@ package com.aliyun.alivcsolution;
 import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
-import android.text.TextUtils;
-
-
 
 import com.aliyun.common.httpfinal.QupaiHttpFinal;
-import com.aliyun.sys.AlivcSdkCore;
-import com.umeng.analytics.MobclickAgent;
-import com.umeng.commonsdk.UMConfigure;
-import com.aliyun.svideo.base.http.EffectService;
 
 /**
  * Created by Mulberry on 2018/2/24.
  */
 public class MutiApplication extends Application {
-    /**
-     * 友盟数据统计key值
-     */
-//    private static final String UM_APP_KEY = "5c6e4e6cb465f5ff47001s0e";
 
     private String mLogPath;
     @Override
@@ -38,13 +27,12 @@ public class MutiApplication extends Application {
         super.onCreate();
         QupaiHttpFinal.getInstance().initOkHttpFinal();
 
-//        AlivcSdkCore.register(getApplicationContext()); // 注册啥？
+//        AlivcSdkCore.register(getApplicationContext()); // 注册啥？如果崩溃再打开，排除了部分so
 //        AlivcSdkCore.setLogLevel(AlivcSdkCore.AlivcLogLevel.AlivcLogWarn);
 //        AlivcSdkCore.setDebugLoggerLevel(AlivcSdkCore.AlivcDebugLoggerLevel.AlivcDLClose);
 
-
-
 //        EffectService.setAppInfo(getResources().getString(R.string.app_name), getPackageName(), BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE);
+
     }
 
 
